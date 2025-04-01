4324ee24b947b42075156c7bd635f9cf3acfd09d
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-04-01 22:49:51 +0800

    Update diy.sh

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -18,6 +18,8 @@ git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-a
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
 #git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
+git clone --depth=1 --single-branch --branch "master" https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
 
 git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
 git clone -b lua --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/alist
