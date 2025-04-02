577e978e91b102bd6b630c396c735fb45c934980
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-04-02 09:13:33 +0800

    Update diy2.sh

--- N1-LEDE/diy2.sh
+++ N1-LEDE/diy2.sh
@@ -12,7 +12,17 @@ cp -r openwrt-passwall-25.3.9-1/luci-app-passwall package/
 rm -rf temp1.zip
 #rm -rf openwrt-passwall-main
 rm -rf openwrt-passwall-25.3.9-1
+#下载使用helloworld依赖库ssr
+wget -qO- https://github.com/fw876/helloworld/archive/master.zip > temp2.zip
+unzip -q temp2.zip
 
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+cp -r helloworld-master/luci-app-ssr-plus package/luci-app-ssr-plus
+cp -r helloworld-master/shadow-tls package/shadow-tls
+cp -r helloworld-master/shadowsocksr-libev package/shadowsocksr-libev
+
+rm -rf temp2.zip
+rm -rf helloworld-master
 
 #下载使用lucky
 wget  -qO- https://codeload.github.com/gdy666/luci-app-lucky/zip/refs/tags/v2.15.8 > lucky.zip
